0a7c0a2ed6576cf879b8f2d02a85a2c51a290f72
Silvain Raveau <silvain@raveau.net>
2025-11-19 14:45:35 +0100

    feat: manage disabled links

--- src/app/components/navigation.tsx
+++ src/app/components/navigation.tsx
@@ -7,18 +7,23 @@ export function NavigationButtons(props: {
 }) {
   return (
     <div className="flex flex-row gap-2 items-center justify-start">
-      <MoveLeft
-        className={cn(
-          "w-4 h-4",
-          props.currentPage === 1
-            ? "text-foreground-disabled"
-            : "text-foreground"
-        )}
-      />
+      <NavigationLink
+        href={`/?page=${props.currentPage - 1}`}
+        disabled={props.currentPage === 1}
+      >
+        <MoveLeft
+          className={cn(
+            "w-4 h-4",
+            props.currentPage === 1
+              ? "text-foreground-disabled"
+              : "text-foreground"
+          )}
+        />
+      </NavigationLink>
       {Array.from({ length: props.numberOfPages }).map((_, pageIndex) => {
         const isCurrentPage = pageIndex + 1 === props.currentPage;
         return (
-          <a
+          <NavigationLink
             key={`navigation-button-${pageIndex + 1}`}
             className={cn(
               "px-4 py-2 rounded-2xl",
@@ -27,19 +32,40 @@ export function NavigationButtons(props: {
                 : "text-foreground"
             )}
             href={`/?page=${pageIndex + 1}`}
+            disabled={isCurrentPage}
           >
             {pageIndex + 1}
-          </a>
+          </NavigationLink>
         );
       })}
-      <MoveRight
-        className={cn(
-          "w-4 h-4",
-          props.currentPage === props.numberOfPages
-            ? "text-foreground-disabled"
-            : "text-foreground"
-        )}
-      />
+      <NavigationLink
+        href={`/?page=${props.currentPage + 1}`}
+        disabled={props.currentPage === props.numberOfPages}
+      >
+        <MoveRight
+          className={cn(
+            "w-4 h-4",
+            props.currentPage === props.numberOfPages
+              ? "text-foreground-disabled"
+              : "text-foreground"
+          )}
+        />
+      </NavigationLink>
     </div>
+  );
+}
+
+function NavigationLink(props: {
+  children: React.ReactNode;
+  href: string;
+  className?: string;
+  disabled?: boolean;
+}) {
+  if (props.disabled)
+    return <span className={props.className}>{props.children}</span>;
+  return (
+    <a href={props.href} className={props.className}>
+      {props.children}
+    </a>
   );
 }
