1ccfff9050cfc64e40c8bae476613ff0205fd92c
Silvain Raveau <silvain@raveau.net>
2025-11-19 10:06:51 +0100

    feat: enhance Home component to handle search parameters and redirect for invalid page numbers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { notFound, redirect } from "next/navigation";
+
 export async function generateMetadata(props: {
   searchParams: Promise<{ page?: string }>;
 }) {
@@ -9,7 +11,16 @@ export async function generateMetadata(props: {
   return { title: `Home - Page ${page}` };
 }
 
-export default function Home() {
+export default async function Home(props: {
+  searchParams: Promise<{ page?: string }>;
+}) {
+  const searchParams = await props.searchParams;
+
+  if (!searchParams.page) redirect("/?page=1");
+
+  const page = Number(searchParams.page);
+  if (Number.isNaN(page) || page <= 0) notFound();
+
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20"></div>
   );
