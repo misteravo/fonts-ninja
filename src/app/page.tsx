778131c35211e616428840da58d3ad9b1f46603e
Silvain Raveau <silvain@raveau.net>
2025-11-19 09:25:07 +0100

    feat: add generateMetadata function to dynamically set page title based on search parameters

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
 import Image from "next/image";
 
+export async function generateMetadata(props: {
+  searchParams: Promise<{ page?: string }>;
+}) {
+  const searchParams = await props.searchParams;
+  return {
+    title: `Home - Page ${searchParams.page ?? "1"}`,
+  };
+}
+
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
