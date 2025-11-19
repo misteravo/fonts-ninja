62925e7da51caee252c837227f22eaebdaeb9d69
Silvain Raveau <silvain@raveau.net>
2025-11-19 14:13:20 +0100

    style: update global styles and navigation buttons to use new button theming

--- src/app/components/navigation.tsx
+++ src/app/components/navigation.tsx
@@ -1,4 +1,5 @@
 import { MoveLeft, MoveRight } from "lucide-react";
+import { cn } from "../utils/classnames";
 
 export function NavigationButtons(props: {
   currentPage: number;
@@ -7,15 +8,23 @@ export function NavigationButtons(props: {
   return (
     <div className="flex flex-row gap-2 items-center justify-start">
       <MoveLeft className="w-4 h-4" />
-      {Array.from({ length: props.numberOfPages }).map((_, pageIndex) => (
-        <a
-          key={`navigation-button-${pageIndex + 1}`}
-          className="bg-primary text-white px-4 py-2 rounded-2xl"
-          href={`/?page=${pageIndex + 1}`}
-        >
-          {pageIndex + 1}
-        </a>
-      ))}
+      {Array.from({ length: props.numberOfPages }).map((_, pageIndex) => {
+        const isCurrentPage = pageIndex + 1 === props.currentPage;
+        return (
+          <a
+            key={`navigation-button-${pageIndex + 1}`}
+            className={cn(
+              "px-4 py-2 rounded-2xl",
+              isCurrentPage
+                ? "bg-button-background text-button-foreground"
+                : "text-foreground"
+            )}
+            href={`/?page=${pageIndex + 1}`}
+          >
+            {pageIndex + 1}
+          </a>
+        );
+      })}
       <MoveRight className="w-4 h-4" />
     </div>
   );
