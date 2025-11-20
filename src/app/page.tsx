dfde35fbb02d2efbd7cedfe870c502228384a6ea
Silvain Raveau <silvain@raveau.net>
2025-11-20 10:59:33 +0100

    refactor: move FamilyCard implementation to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { Card } from "@/components/card";
+import { NavigationLink } from "@/components/link";
+import { SvgRenderer } from "@/components/svg-renderer";
 import { notFound, redirect } from "next/navigation";
-import { FamilyCard } from "../components/family-card";
 import { NavigationButtons } from "../components/navigation";
-import { FontFamiliesResponse } from "../types/font-family";
+import { FontFamiliesResponse, FontFamily } from "../types/font-family";
 
 export async function generateMetadata(props: {
   searchParams: Promise<{ page?: string }>;
@@ -45,5 +47,35 @@ export default async function Home(props: {
         className="px-4"
       />
     </div>
+  );
+}
+
+function FamilyCard({ family }: { family: FontFamily }) {
+  const svg = family.images.alphabet.svg.replace(
+    /<g fill="[^"]*"/g,
+    `<g class="fill-card-foreground"`
+  );
+  return (
+    <NavigationLink href={family.url}>
+      <Card className="flex flex-col justify-end gap-8">
+        <SvgRenderer svg={svg} />
+        <div className="flex flex-row justify-between w-full">
+          <div className="flex flex-col">
+            <p className="font-bold">{family.name}</p>
+            <p>{family.foundry.name}</p>
+          </div>
+          <div className="flex flex-col">
+            <p>
+              {family.price ? (
+                `From ${family.price?.formatedPrice}`
+              ) : (
+                <>&nbsp;</>
+              )}
+            </p>
+            <p>{family.totalFonts} styles</p>
+          </div>
+        </div>
+      </Card>
+    </NavigationLink>
   );
 }
