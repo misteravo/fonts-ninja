d9f46d0bf44678ddca92675a93b0db714f374afe
Silvain Raveau <silvain@raveau.net>
2025-11-20 10:02:18 +0100

    feat: implement FontPage component to display font details and SVG rendering

--- src/app/font/[id]/page.tsx
+++ src/app/font/[id]/page.tsx
@@ -1,3 +1,29 @@
-export default function FontPage() {
-  return <div></div>;
+import { SvgRenderer } from "@/components/svg-renderer";
+import { FontFamilyDetailsResponse } from "@/types/font-family";
+
+export default async function FontPage() {
+  const fontResponse = (await fetch(
+    `http://localhost:3000/api/familyDetails`
+  ).then((response) => response.json())) as FontFamilyDetailsResponse;
+
+  const pangramSvg = fontResponse.images.pangram.svg.replace(
+    /<g fill="[^"]*"/g,
+    `<g class="fill-card-foreground"`
+  );
+
+  return (
+    <div className="grid grid-cols-2 gap-4">
+      <div className="col-span-1">
+        <SvgRenderer svg={pangramSvg} />
+      </div>
+      <div className="col-span-1">
+        <h1 className="font-bold text-card-foreground text-2xl">
+          {fontResponse.name}
+        </h1>
+        <h2 className="text-card-foreground text-lg">
+          {fontResponse.foundry.name}
+        </h2>
+      </div>
+    </div>
+  );
 }
