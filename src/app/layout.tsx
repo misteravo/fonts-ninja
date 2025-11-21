ae1a1d51606afb4da098422946aa85f63e7a6695
Silvain Raveau <silvain@raveau.net>
2025-11-21 16:51:30 +0100

    fix: integrate theme class dark to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Logo from "../assets/logo";
 import "./globals.css";
 import Link from "next/link";
 import { cookies } from "next/headers";
+import { cn } from "@/utils/classnames";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -21,8 +22,11 @@ export default async function RootLayout({
   if (theme && !["light", "dark"].includes(theme)) theme = undefined;
 
   return (
-    <html lang="en" className={inter.className}>
-      <ThemeProvider theme={theme as ThemeValue}>
+    <ThemeProvider theme={theme as ThemeValue}>
+      <html
+        lang="en"
+        className={cn(inter.className, theme === "dark" && "dark")}
+      >
         <body className="px-14 pt-9.25 pb-46.25 flex flex-col gap-14">
           <header className="flex flex-row items-center justify-between">
             <Link href="/">
@@ -32,7 +36,7 @@ export default async function RootLayout({
           </header>
           {children}
         </body>
-      </ThemeProvider>
-    </html>
+      </html>
+    </ThemeProvider>
   );
 }
