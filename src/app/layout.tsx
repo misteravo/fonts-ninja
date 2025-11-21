1b73be8e97b7497852213aae360c2bb1389cf618
Silvain Raveau <silvain@raveau.net>
2025-11-21 15:28:38 +0100

    style: adjust body padding in RootLayout for improved layout consistency

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={inter.className}>
       <ThemeProvider>
-        <body className="px-12 py-10 flex flex-col gap-14">
+        <body className="px-14 py-9.25 flex flex-col gap-14">
           <header className="flex flex-row items-center justify-between">
             <Link href="/">
               <Logo />
